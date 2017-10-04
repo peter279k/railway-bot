b0aa847ab2772a88e069d7ad066738878d5ff89c
Henrique Duarte <bick3@terra.com.br>
2017-10-03 23:24:10 -0300

    Add PHPUnit

---
+++ tests/TestBase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace railway\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class TestBase extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+}
---
+++ tests/Unit/initTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace railway\Tests\Unit;
+
+use railway\Tests\TestBase;
+
+class initTest extends TestBase
+{
+    /** @test */
+    function init_phpunit()
+    {
+        $this->assertTrue(true);
+    }
+}
